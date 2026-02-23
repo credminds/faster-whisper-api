8167be8f401af306fc8bb52c0dbd30c2ea2a0128
Ahmad Ashfaq <spoiledwit@gmail.com>
2026-02-23 10:21:01 +0500

    fix: fixed couple of minor/major issues

--- main.py
+++ main.py
@@ -65,4 +65,4 @@ async def transcribe_stream(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8004)
